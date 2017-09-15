4254cc1fdce9d029f027c8c7507853e508af488f
Chris Cardle <cdcardle@gmail.com>
2017-09-15 13:15:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ var recipes = {}
 function updateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign({}, obj, {[key]: value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return obj[key] = value
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = delete obj.key
+  return newObj
+}
+
