5651f35027d73a17ddcaa192ace99237de1b1413
Chris Cardle <cdcardle@gmail.com>
2017-09-15 12:27:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  Object.assign({}, obj, [key], value)
+  return obj
+}
+
+function destructivelyUpdateObjectWithKeyAndValue()
